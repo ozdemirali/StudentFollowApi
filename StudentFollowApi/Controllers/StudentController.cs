2f93993da716de6b07801a9555d6d06a423f7081
Ali <ozdemirali@hotmail.com>
2022-01-28 00:12:03 +0300

    StudentController is created.

---
+++ StudentFollowApi/Controllers/StudentController.cs
@@ -0,0 +1,68 @@
+﻿using StudentFollowApi.Context;
+using StudentFollowApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentFollowApi.Controllers
+{
+    public class StudentController : ApiController
+    {
+        public IHttpActionResult GetAllStudents()
+        {
+            IList<StudentViewModel> students = null;
+
+            using (var ctx = new StudentFollowDbContext())
+            {
+                students = ctx.Students
+                            .Select(s => new StudentViewModel()
+                            {
+                                Id = s.Id,
+                                NameAndSurname = s.NameAndSurname,
+                                UseMedicine = s.UseMedicine,
+                                NumberOfBrotherAndSister = s.NumberOfBrotherAndSister,
+                                ContinuallyIllness = s.ContinuallyIllness,
+                                PastIlness = s.PastIlness,
+                                Weight = s.Weight,
+                                Size = s.Size,
+                                Phone = s.Phone,
+                                Number = s.Number,
+                                UseProthes = s.UseProthes,
+                                PastOperation = s.PastOperation,
+                                Accident = s.Accident,
+                                FamilyIncomeMoney = s.FamilyIncomeMoney,
+                                FamilyIncomeStatusId = s.FamilyIncomeStatusId,
+                                TypeOfDisability = s.TypeOfDisability,
+                                Scheck = s.Scheck,
+                                PlaceOfBirth = s.PlaceOfBirth,
+                                DateOfBirth = s.DateOfBirth,
+                                RecordNumberOfIdentityCard = s.RecordNumberOfIdentityCard,
+                                GivenDateOfIdentityCard = s.GivenDateOfIdentityCard,
+                                RentOfHouse = s.RentOfHouse,
+                                HaveOwnHouse = s.HaveOwnHouse,
+                                Working = s.Working,
+                                OutsideFromFamily = s.OutsideFromFamily,
+                                CameFromAbroad = s.CameFromAbroad,
+                                Scholarship = s.Scholarship,
+                                HomeHeatingId = s.HomeHeatingId,
+                                BranchId = s.BranchId,
+                                ClassroomId = s.ClassroomId,
+                                WhitWhomLiveId = s.WhitWhomLiveId,
+                                BooldGroupId = s.BooldGroupId,
+                                HowToGetSchoolId = s.HowToGetSchoolId,
+                                GuardianId = s.GuardianId
+                            }).ToList<StudentViewModel>();
+                if (students.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(students);
+
+            }
+        }
+    }
+}
--- StudentFollowApi/ViewModels/StudentViewModel.cs
+++ StudentFollowApi/ViewModels/StudentViewModel.cs
@@ -1,63 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace StudentFollowApi.Models
+namespace StudentFollowApi.ViewModels
 {
-    public class Student
+    public class StudentViewModel
     {
-        [MaxLength(11)]
         public string Id { get; set; }
 
-        [MaxLength(50)]
         public string NameAndSurname { get; set; }
-        
-        [MaxLength(50)]
+
         public string UseMedicine { get; set; }
         public byte NumberOfBrotherAndSister { get; set; }
-        
-        [MaxLength(50)]
+
         public string ContinuallyIllness { get; set; } // Sürekli Hasatalığı
-        
-        [MaxLength(50)]
+
         public string PastIlness { get; set; }
 
         public decimal Weight { get; set; }
 
         public decimal Size { get; set; } //Boyu
 
-        [MaxLength(11)]
         public string Phone { get; set; }
 
-        [MaxLength(4)]
         public string Number { get; set; }
 
-        [MaxLength(4)]
         public string UseProthes { get; set; }  //Kullandığı Protez
 
-        [MaxLength(50)]
         public string PastOperation { get; set; }
 
-        [MaxLength(50)]
         public string Accident { get; set; } //Geçirdiği Kazalar
 
         public decimal FamilyIncomeMoney { get; set; } // Ailenin Kazandığı Para Miktarı
 
-        [MaxLength(50)]
         public string TypeOfDisability { get; set; } //Özür Türü
 
         public byte Scheck { get; set; } // Sosyal Hiz. Çocuk Esirgeme Kurumuna Tabi mi?
 
-        [MaxLength(50)]
         public string PlaceOfBirth { get; set; } //Doğum Yeri
 
         public DateTime DateOfBirth { get; set; }
 
-        [MaxLength(50)]
-        public string RecordNumberOfIdentityCard { get; set; }
-        public DateTime GivenDateOfIdentityCard { get;set; }
+        public string RecordNumberOfIdentityCard { get; set; } // Nüfüz Cüzdanı Kayıt Tarihi
+
+        public DateTime GivenDateOfIdentityCard { get; set; } // Nüfüz Cüzdanı Kayıt Tarihi
 
         public bool RentOfHouse { get; set; }  //Evi Kira mı?
 
@@ -72,39 +59,21 @@ namespace StudentFollowApi.Models
         public bool Scholarship { get; set; } //Burslu mu?
 
         public byte HomeHeatingId { get; set; }
-        public HomeHeating HomeHeating { get; set; }
-
-
         public byte BranchId { get; set; }
-        public Branch Branch { get; set; }
 
         public byte ClassroomId { get; set; }
-        public Classroom Classroom { get; set; }
 
         public byte WhitWhomLiveId { get; set; }
-        public WhitWhomLive WhitWhomLive { get; set; }
 
         public byte BooldGroupId { get; set; }
-        public BloodGroup BloodGroup { get; set; }
 
         public byte ReligionId { get; set; }
-        public Religion Religion { get; set; }
 
 
         public byte HowToGetSchoolId { get; set; }
-        public HowToGetSchool HowToGetSchool { get; set; }
 
         public byte FamilyIncomeStatusId { get; set; }
-        public FamilyIncomeStatus FamilyIncomeStatus { get; set; }
 
-        [MaxLength(11)]
         public string GuardianId { get; set; }
-        public Guardian Guardian { get; set; }
-
-
-        public ICollection<Sibling> Siblings { get; set; }
-        public ICollection<Family> Families { get; set; }
-
-
     }
 }
